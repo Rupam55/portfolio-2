d8d7924b7d3a40608827d46c4d1520beba73156c
rupam karmakar <rupamkarmakar911@gmail.com>
2020-11-29 02:27:10 +0530

    second commit

--- javascript/app.js
+++ javascript/app.js
@@ -19,24 +19,34 @@ $(document).ready(function () {
 $(function () {
   $(window).bind("resize", function () {
     console.log($(this).width());
-    if ($(this).width() < 500) {
+    if ($(this).width() <= 500) {
       $("#left_nav").removeClass("pad");
       $("#head").addClass("small_h1");
-      $("#name").addClass("small_p")
-      $(".item").addClass("small_i");
+      $("#name").addClass("small_p");
+      $(".items").removeClass("gen_p");
+      $(".items").addClass("small_i");
     } else {
       $("#left_nav").addClass("pad");
+      $(".items").addClass("gen_p");
+      $("#head").removeClass("small_h1");
+      $("#name").removeClass("small_p");
+      $(".items").removeClass("small_i");
     }
   });
 });
 
 $(function () {
-  if ($(window).width()<=500) {
+  if ($(window).width() <= 500) {
     $("#left_nav").removeClass("pad");
     $("#head").addClass("small_h1");
-    $("#name").addClass("small_p")
-    $(".item").addClass("small_i");
+    $("#name").addClass("small_p");
+    $(".items").removeClass("gen_p");
+    $(".items").addClass("small_i");
   } else {
     $("#left_nav").addClass("pad");
+    $(".items").addClass("gen_p");
+    $("#head").removeClass("small_h1");
+    $("#name").removeClass("small_p");
+    $(".items").removeClass("small_i");
   }
 });
